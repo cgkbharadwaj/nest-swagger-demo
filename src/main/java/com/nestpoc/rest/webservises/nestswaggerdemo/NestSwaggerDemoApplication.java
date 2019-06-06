d762cb528f11a16681afa831f69e68a71e6462b4
cgkbharadwaj <cgkbharadwaj@gmail.com>
2019-06-06 16:22:16 +0100

    first commit

---
+++ src/main/java/com/nestpoc/rest/webservises/nestswaggerdemo/NestSwaggerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nestpoc.rest.webservises.nestswaggerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestSwaggerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestSwaggerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestpoc/rest/webservises/nestswaggerdemo/NestSwaggerDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nestpoc.rest.webservises.nestswaggerdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NestSwaggerDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
